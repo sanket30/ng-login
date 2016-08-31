25b66012e07370482ab3d3a68676b29fa4982409
Sanket Tajane <sanket.tajane@gmail.com>
2016-08-31 00:32:33 -0500

    add qualify candidate page

--- app/qualify-candidate/qualify-candidate.controller.js
+++ app/qualify-candidate/qualify-candidate.controller.js
@@ -8,6 +8,50 @@
     // @ngInject
     function QualifyCandidateController() {
         var vm = this;
+        vm.isQualified = true;
+        vm.checkValue = checkValue;
+
+        vm.questions = [
+            {
+                text: 'Does the candidate currently have a valid H1B?',
+                expectedAnswer: 1
+            },
+            {
+                text: 'Does the current H1 expire within the next two months?',
+                expectedAnswer: 0
+            },
+            {
+                text: 'Has the Candidate been on H1 for more than 5 years?',
+                expectedAnswer: 0
+            },
+            {
+                text: 'Has anyone started this Candidates Green Card process yet?',
+                expectedAnswer: 0
+            },
+            {
+                text: 'Is the candidate currently engaged in project?',
+                expectedAnswer: 0
+            }
+        ];
+
+        function checkValue() {
+            vm.isQualified = true;
+            _.each(vm.questions, function (data) {
+                if (data.submittedAnswer && data.expectedAnswer != data.submittedAnswer) {
+                    vm.isQualified = false;
+                    return;
+                }
+            });
+
+            for (var i = 0; i < vm.questions.length; i++) {
+                if (vm.questions[i].expectedAnswer == vm.questions[i].submittedAnswer) {
+                    vm.isSureQualified = true;
+                } else {
+                    vm.isSureQualified = false;
+                    return;
+                }
+            }
+        }
 
     }
 }(angular));
