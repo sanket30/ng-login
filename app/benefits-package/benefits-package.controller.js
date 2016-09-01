dd9165f7a2afc9b111a5503a0ccfb1c634eb5c08
Sanket Tajane <sanket.tajane@gmail.com>
2016-09-01 16:01:47 -0500

    added send benefit package page

--- app/benefits-package/benefits-package.controller.js
+++ app/benefits-package/benefits-package.controller.js
@@ -6,8 +6,23 @@
         .controller('BenefitsPackageController', BenefitsPackageController);
 
     // @ngInject
-    function BenefitsPackageController() {
+    function BenefitsPackageController($window) {
         var vm = this;
+        var subject = 'Egen Solutions/Insight Global benefits';
+        var body = 'Thank you for taking the time to speak  with us. Please find General information about Egen Solutions and their H1 transfer process,' +
+            ' along with Healthcare Benefits attached. If you have any questions regarding this, please contact Trevor, who’s information can be found below.' +
+            'Trevor Benson- Sales/Partnerships' +
+            'trevor.benson@egeni.com (815) 762-8659' +
+            'www.egen.solutions  Naperville- IL';
+
+        vm.send = send;
+
+        function send() {
+            $window.location.href =
+                'mailto:' + vm.candidateEmail +
+                '?subject=' + subject +
+                '&body=' + (vm.message ? vm.message : body);
+        }
 
     }
 }(angular));
