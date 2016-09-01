66ccf2668fc9973e05e2809b3781889673be7a25
Sanket Tajane <sanket.tajane@gmail.com>
2016-09-01 12:16:06 -0500

    add rate calculator logic

--- app/rate-calculator/rate-calculator.controller.js
+++ app/rate-calculator/rate-calculator.controller.js
@@ -8,6 +8,19 @@
     // @ngInject
     function RateCalculatorController() {
         var vm = this;
+        vm.calculateRate = calculateRate;
+
+        vm.finalRate = 0;
+        vm.baseRate = 0;
+        vm.isRelocationNeeded = true;
+
+        function calculateRate() {
+            if (vm.baseRate) {
+                vm.finalRate = 1.1 * ( (vm.baseRate * 1.1) + 4.350 + (vm.isRelocationNeeded ? 1 : 0) + 1 );
+            } else {
+                vm.finalRate = 0;
+            }
+        }
 
     }
 }(angular));
